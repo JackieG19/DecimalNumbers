7705ef2ac30dc5737651d3623814693ddf3d2dba
Jackie <jacgut19@gmail.com>
2017-12-04 14:43:30 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//Create Decimal Numbers with JavaScript
+
+var myDecimal = 2.8;
+
+output: myDecimal = 2.8
+
+
+//Multiply Two Decimals with JavaScript
+
+var product = 2.0 * 2.5;
+
+output: product = 5
+
+
+//Divide one Decimal by Another with JavaScript
+
+var quotient = 4.4 / 2.0;
+
+output: quotient = 2.2
+
+
+//Finding a Remainder in JavaScript
+
+Math.floor (Math.random(11 % 3));
+var remainder = 2 % 11;
+
+output: remainder = 2
+
